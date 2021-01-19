262db3e80d0b76fc04a0ee6d1984247a82bfbf49
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-19 10:55:50 -0700

    Create postOrder api post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,23 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrder = (name, ingredients) => {
+  let postData = {
+    "id": Date.now(),
+    "name": name,
+    "ingredients": ingredients
+  };
+
+  let postString = JSON.stringify(postData)
+
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    body: postString,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+}
+
