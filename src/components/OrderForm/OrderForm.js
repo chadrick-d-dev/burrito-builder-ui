e284f9810f05d6f49f2321c6b6fd024edabca039
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-19 10:55:17 -0700

    Refactor & Create handle NameChange/IngredientsChange/Submit & import postOrder

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { postOrder } from '../../apiCalls.js';
 class OrderForm extends Component {
   constructor(props) {
     super();
@@ -7,13 +7,41 @@ class OrderForm extends Component {
     this.state = {
       name: '',
       ingredients: []
-    };
+    }
   }
 
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     this.clearInputs();
+  }
+
+  handleNameChange = (e) => {
+    let name = e.target.name
+    this.setState({ [name]: e.target.value })
+  }
+
+  handleIngredientChange= (e) => {
+    const name = e.target.name
+    let ingredientsArray = this.state.ingredients;
+    
+    if (ingredientsArray.length < 1) {
+      e.preventDefault()
+      ingredientsArray.push(name);
+      this.setState({ ingredients: ingredientsArray })
+    } else if ((ingredientsArray.length > 0) && !ingredientsArray.includes(name)) {
+      e.preventDefault()
+      ingredientsArray.push(name);
+      this.setState({ ingredients: ingredientsArray })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.name && this.state.ingredients.length > 0) {
+      postOrder(this.state.name, this.state.ingredients)
+      this.clearInputs()
+    }
   }
 
   clearInputs = () => {
@@ -21,10 +49,10 @@ class OrderForm extends Component {
   }
 
   render() {
-    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
+    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream']
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} onClick={(e) => this.handleIngredientChange(e)}>
           {ingredient}
         </button>
       )
